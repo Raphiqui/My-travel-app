43910dd316adc7aadbe2fe949d5143bbbe529101
Norsse <raphael.pastre@gmail.com>
2019-09-10 15:55:14 +0200

    Add sticky header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Menu, Icon } from 'semantic-ui-react'
+import { Menu, Icon, Sticky } from 'semantic-ui-react'
 
 export default class Header extends Component {
     state = {};
@@ -12,25 +12,27 @@ export default class Header extends Component {
         const { activeItem } = this.state;
 
         return (
-            <Menu size={"massive"}>
-                <Menu.Item
-                    name='Home page'
-                    active={activeItem === 'Home page'}
-                    onClick={this.handleItemClick}
-                >
-                    <Icon name={"home"}/>
-                    Home page
-                </Menu.Item>
+            <Sticky>
+                <Menu size={"massive"}>
+                    <Menu.Item
+                        name='Home page'
+                        active={activeItem === 'Home page'}
+                        onClick={this.handleItemClick}
+                    >
+                        <Icon name={"home"}/>
+                        Home page
+                    </Menu.Item>
 
-                <Menu.Item
-                    name='About'
-                    active={activeItem === 'About'}
-                    onClick={this.handleItemClick}
-                >
-                    <Icon name={"archive"}/>
-                    About
-                </Menu.Item>
-            </Menu>
+                    <Menu.Item
+                        name='About'
+                        active={activeItem === 'About'}
+                        onClick={this.handleItemClick}
+                    >
+                        <Icon name={"archive"}/>
+                        About
+                    </Menu.Item>
+                </Menu>
+            </Sticky>
         )
     }
 }
