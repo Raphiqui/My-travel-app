d3d36b828f267f4a39dfd24d40e32a42072b1f29
Norsse <raphael.pastre@gmail.com>
2019-09-10 15:37:03 +0200

    Add menu Home page and About page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Menu } from 'semantic-ui-react'
 
-function Header() {
-    return (
-        <header className="App-header">
-            <p>Hello there</p>
-            <p>I'm bob</p>
-        </header>
-    );
+export default class Header extends Component {
+    state = {};
+
+    handleItemClick = (e, { name }) => {
+        this.setState({ activeItem: name });
+    };
+
+    render() {
+        const { activeItem } = this.state;
+
+        return (
+            <Menu size={"massive"}>
+                <Menu.Item
+                    name='Home page'
+                    active={activeItem === 'Home page'}
+                    onClick={this.handleItemClick}
+                >
+                    Home page
+                </Menu.Item>
+
+                <Menu.Item
+                    name='About'
+                    active={activeItem === 'About'}
+                    onClick={this.handleItemClick}
+                >
+                    About
+                </Menu.Item>
+            </Menu>
+        )
+    }
 }
-
-export default Header;
