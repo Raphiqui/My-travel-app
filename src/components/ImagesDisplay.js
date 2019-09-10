e223e28e4916f6145180fbbaa2d6789e1a577abb
Norsse <raphael.pastre@gmail.com>
2019-09-10 16:17:59 +0200

    Linked imagesDisplay and App

---
+++ src/components/ImagesDisplay.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { Menu, Icon, Sticky } from 'semantic-ui-react'
+
+export default class ImagesDisplay extends Component {
+    state = {};
+
+    render() {
+
+        return (
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
