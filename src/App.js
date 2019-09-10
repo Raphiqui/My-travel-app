85ac1c4ebb2a7004c117a2a7b9a693b41f0fa1b2
Norsse <raphael.pastre@gmail.com>
2019-09-10 15:27:42 +0200

    Linked App.js and Header.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello there</p>
-      </header>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="App-header">
+            <p>Hello there</p>
+            <p>I'm bob</p>
+        </header>
+    );
+}
+
+export default Header;
