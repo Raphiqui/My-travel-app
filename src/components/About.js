621de527a9404a7778ec60e421375048162c33d4
Norsse <raphael.pastre@gmail.com>
2019-09-14 10:26:06 +0200

    Update About | some refactoring

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,32 @@
 import React, {Component} from 'react';
-import {Modal, Icon, Menu} from 'semantic-ui-react'
+import {Icon, Menu} from 'semantic-ui-react'
+import { notification } from 'antd';
 
 export default class About extends Component {
     state = {};
 
+    openNotification = () => {
+        notification.open({
+            message: 'Purpose of this app',
+            duration : 5,
+            description:
+                'This app has been created in order to keep going my training about Reactjs after my ending studies internship. ' +
+                'It purpose is to show few of my trips across the world.',
+            icon: <Icon name="globe" style={{ color: '#2ab9e9' }} />,
+            style: {
+                width: 600,
+                marginLeft: 335 - 600,
+            },
+            onClick: () => {
+                console.log('Notification Clicked!');
+            },
+        });
+    };
+
     render() {
 
         return (
-            <Modal closeIcon trigger={<Menu.Item name='About' > <Icon name={"archive"}/> About </Menu.Item>}>
-
-                <Modal.Header>About my travel app</Modal.Header>
-                <Modal.Content>
-                    <p>Content is coming</p>
-                </Modal.Content>
-
-            </Modal>
+            <Menu.Item name='About' onClick={this.openNotification}> <Icon name={"archive"}/> About </Menu.Item>
         )
     }
 }
