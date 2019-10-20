d5dc99e53144b638af81f385e6e5ae1d744e9f10
Norsse <raphael.pastre@gmail.com>
2019-10-20 17:44:26 +0200

    add flags

--- src/components/ImageDisplayItem1.js
+++ src/components/ImageDisplayItem1.js
@@ -11,21 +11,22 @@ export default class ImagesDisplayItem1 extends Component {
 
     render() {
         const {cardSelected} = this.props;
-
+        console.log('BO:', cardSelected)
+        //TODO tweak Egypt by Jordanie and add another card with Egypt
         return (
             <div>
                 <Segment style={{ padding: '8em 0em', color:"white"}} vertical>
                     <Grid container stackable verticalAlign='middle'>
                         <Grid.Row>
                             <Grid.Column width={8}>
-                                <Header as='h2' style={{ fontSize: '2em' }}>
+                                <Header as='h2' style={{ fontSize: '2em', color: 'white' }}>
                                     {cardSelected['name'] === cardSelected['countryName']
                                         ? cardSelected["name"]
                                         : cardSelected["name"] + " (" + cardSelected["countryName"] + ")"
                                     }
                                 </Header>
-                                <Header style={{ fontSize: '2em' }}>
-                                    <h2>About my journey there</h2>
+                                <Header style={{ fontSize: '2em'}}>
+                                    <h2 style={{ color: 'white'}}>About my journey there</h2>
                                 </Header>
                                 <p style={{ fontSize: '1.33em', color: 'white' }}>
                                     {/*Description of what you've done there and for how long*/}
@@ -42,12 +43,12 @@ export default class ImagesDisplayItem1 extends Component {
                             <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
                                 <Header as='h3' style={{ fontSize: '2em', color: 'white' }}>
                                     {/*description of the country or town or ...*/}
-                                    Title will change
+                                    Description
                                 </Header>
                                 <p style={{ fontSize: '1.33em' }}>{cardSelected['description']}</p>
                             </Grid.Column>
                             <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-                                <Header as='h3' style={{ fontSize: '2em' }}>
+                                <Header as='h3' style={{ fontSize: '2em', color: 'white' }}>
                                     {/*Memories*/}
                                     What I'll remember
                                 </Header>
@@ -59,16 +60,19 @@ export default class ImagesDisplayItem1 extends Component {
                             </Grid.Column>
                         </Grid.Row>
                     </Grid>
-                    <Grid>
-                        <Grid.Row textAlign="center">
-                            <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-                                <Header as='h3' style={{ fontSize: '2em' }}>
-                                    {/*Ending world, would I like to go back there, why ? */}
-                                    The ending word
-                                </Header>
-                                <p style={{ fontSize: '1.33em' }}>Very difficult to find a good conclusion</p>
-                            </Grid.Column>
-                        </Grid.Row>
+                    <Grid centered columns={5}>
+                        <Grid.Column>
+                            <Image bordered rounded size='medium' src={process.env.PUBLIC_URL + cardSelected['flag']} />
+                        </Grid.Column>
+                        {/*<Grid.Row textAlign="center">*/}
+                            {/*<Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>*/}
+                                {/*<Header as='h3' style={{ fontSize: '2em', color: 'white' }}>*/}
+                                    {/*/!*Ending world, would I like to go back there, why ? *!/*/}
+                                    {/*The ending word*/}
+                                {/*</Header>*/}
+                                {/*<p style={{ fontSize: '1.33em' }}>Very difficult to find a good conclusion</p>*/}
+                            {/*</Grid.Column>*/}
+                        {/*</Grid.Row>*/}
                     </Grid>
                 </Segment>
             </div>
