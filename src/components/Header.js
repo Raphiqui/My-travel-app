d5fc0869b4a66413b38894de7d26ea561e6c6374
Norsse <raphael.pastre@gmail.com>
2019-10-21 08:39:41 +0200

    tweak header behavior

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ export default class Header extends Component {
         const { activeItem, handleItemClick } = this.props;
 
         return (
-            <Menu fluid widths={3} fixed='top' size={"massive"} style={{backgroundColor: "#5E7FA8"}}>
+            <Menu fluid widths={2} fixed='top' size={"massive"} style={{backgroundColor: "#5E7FA8"}}>
                 <Menu.Item
                     name='Home page'
                     active={activeItem === 'Home page'}
@@ -25,7 +25,7 @@ export default class Header extends Component {
 
                 <About/>
 
-                <Menu.Item as={"h3"}><b>My travel app</b></Menu.Item>
+                {/*<Menu.Item as={"h3"}><b>My travel app</b></Menu.Item>*/}
 
             </Menu>
         )
